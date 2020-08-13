38faabd8f6762ce392164e0622afaeafa06eae32
CGFighter <catgirlfighter@gmail.com>
2020-08-13 05:31:08 +0300

    1.0.14

---
+++ Source/MicroDesignations12/MicroDesignations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HarmonyLib;
+using Verse;
+using System.Reflection;
+
+namespace MicroDesignations
+{
+    [StaticConstructorOnStartup]
+    public class MicroDesignations : Mod
+    {
+        public static FieldInfo LdisallowedFilters = null;
+        public MicroDesignations(ModContentPack content) : base(content)
+        {
+            LdisallowedFilters = AccessTools.Field(typeof(ThingFilter), "disallowedSpecialFilters");
+            if (LdisallowedFilters == null) throw new Exception("Couldn't get ThingFilter.disallowedSpecialFilters");
+            var harmony = new Harmony("net.avilmask.rimworld.mod.MicroDesignations");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            GetSettings<Settings>();
+        }
+
+        public void Save()
+        {
+            LoadedModManager.GetMod<MicroDesignations>().GetSettings<Settings>().Write();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "MicroDesignations";
+        }
+
+        public override void DoSettingsWindowContents(UnityEngine.Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
+        }
+    }
+}
---
+++ Source/MicroDesignations12/Patches/JobPatches.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+using HarmonyLib;
+using Verse.AI;
+
+namespace MicroDesignations
+{
+    static class Job_Patch
+    {
+        [HarmonyPatch(typeof(Job), nameof(Job.ExposeData))]
+        static class Job_ExposeData_MicroDesignationsPatch
+        {
+            static void Prefix(Job __instance)
+            {
+                if (__instance.bill == null || __instance.bill.billStack == null) Scribe_Deep.Look(ref __instance.bill, "billSaved"); 
+            }
+        }
+    }
+}
